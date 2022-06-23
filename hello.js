b6a49eadcd7a2f7ae3cdc13db9549dac0b05ca5a
wambuingare <wambuinancyngare@gmail.com>
2022-06-23 19:52:59 +0300

    website

---
+++ hello.js
@@ -0,0 +1,3 @@
+function display() {  
+    alert("This is a website about an organization that is based in Hindi-Lamu, that has contributed to making the girl child aware of her rights and also has helped the girls with other basic needs. Apart from that, the organization has helped the community during the pandemic by providing masks, and sanitizers and also spreading awareness about the covid outbreak, especially in the rural areas. ");  
+    }
